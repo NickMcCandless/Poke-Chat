540355c4381c1d1db936e24448e4ddeb2b4331fd
Byteslick <byteslick@gmail.com>
2016-07-14 15:43:35 +0530

    Initial Commit

---
+++ Poke Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Poke Chat
+//
+//  Created by Prakhar Singh on 14/07/16.
+//  Copyright © 2016 TAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Poke Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Poke Chat
+//
+//  Created by Prakhar Singh on 14/07/16.
+//  Copyright © 2016 TAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
